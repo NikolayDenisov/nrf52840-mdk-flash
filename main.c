e57dbc34f7d772e7933b51b3d97cc40b91fbfdf0
Николай Денисов <nickdenisov@protonmail.com>
2025-01-04 00:01:03 +0500

    Fixed: comments

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
+
 #include "nrf52840.h"
 #include "nrf_delay.h"
+#include "nrf_drv_uart.h"
 
-#define FLASH_START_ADDR  0x3F000  // адрес страницы для записи (выберите подходящий под вашу задачу)
-#define FLASH_PAGE_SIZE   4096     // размер страницы флеш-памяти
+#define FLASH_START_ADDR 0x3F000  // адрес страницы для записи
+#define FLASH_PAGE_SIZE 4096      // размер страницы флеш-памяти
 
-// Пример IP-адреса в формате uint32_t (например, 192.168.1.1)
+// IP-адрес в формате uint32_t
 uint32_t ip_address = 0xC0A80101;  // 0xC0 = 192, 0xA8 = 168, 0x01 = 1, 0x01 = 1
+
+nrf_drv_uart_t m_uart = NRF_DRV_UART_INSTANCE(0);
 
 void flash_write(uint32_t address, uint32_t value) {
     // Разблокируем флеш-память для записи
-    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;  // WEN = Write enable
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;            // WEN = Write enable
     while (NRF_NVMC->READY == NVMC_READY_READY_Busy);  // Ждём, пока контроллер готов
 
     // Записываем значение
-    *(uint32_t*)address = value;
+    *(uint32_t *)address = value;
     while (NRF_NVMC->READY == NVMC_READY_READY_Busy);  // Ждём завершения записи
 
     // Блокируем запись
@@ -39,7 +43,34 @@ void flash_page_erase(uint32_t address) {
 
 uint32_t flash_read(uint32_t address) {
     // Чтение из флеш-памяти осуществляется напрямую
-    return *(uint32_t*)address;
+    return *(uint32_t *)address;
+}
+
+void uart_init(void) {
+    nrf_drv_uart_config_t uart_config = NRF_DRV_UART_DEFAULT_CONFIG;
+    uart_config.baudrate = NRF_UART_BAUDRATE_115200;
+    uart_config.pseltxd = 20;
+    uart_config.pselrxd = 19;
+
+    nrf_drv_uart_init(&m_uart, &uart_config, NULL);
+}
+
+void uart_put_string(const char *str) {
+    while (*str) {
+        nrf_drv_uart_tx(&m_uart, (uint8_t *)str++, 1);
+    }
+}
+
+void print_ip_address(uint32_t ip_addr) {
+    uint8_t byte1 = (ip_addr >> 24) & 0xFF;
+    uint8_t byte2 = (ip_addr >> 16) & 0xFF;
+    uint8_t byte3 = (ip_addr >> 8) & 0xFF;
+    uint8_t byte4 = ip_addr & 0xFF;
+
+    char ip_str[16];
+    snprintf(ip_str, sizeof(ip_str), "%d.%d.%d.%d\n", byte1, byte2, byte3, byte4);
+
+    uart_put_string(ip_str);
 }
 
 int main(void) {
@@ -48,10 +79,11 @@ int main(void) {
 
     // Записываем IP-адрес в первую ячейку страницы
     // flash_write(FLASH_START_ADDR, ip_address);
-
+    uart_init();
     while (true) {
-        nrf_delay_us(5000);
+        nrf_delay_ms(5000);
         uint32_t read_ip_address = flash_read(FLASH_START_ADDR);
+        print_ip_address(read_ip_address);
         if (read_ip_address == ip_address) {
             __NOP();
         }
