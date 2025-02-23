7e0dc43df112fca9ee853d81e9eaaef06188c6fa
Николай Денисов <nickdenisov@protonmail.com>
2025-02-24 00:04:26 +0500

    Fixed: working example

--- main.c
+++ main.c
@@ -6,11 +6,9 @@
 #include "nrf_drv_uart.h"
 
 #define FLASH_START_ADDR 0x3F000  // адрес страницы для записи
-#define FLASH_PAGE_SIZE 4096      // размер страницы флеш-памяти
 
 // IP-адрес в формате uint32_t
-uint32_t ip_address = 0xC0A80101;    // 0xC0 = 192, 0xA8 = 168, 0x01 = 1, 0x01 = 1
-uint32_t ip_address_2 = 0xC0A80102;  // 192.168.1.2
+uint32_t ip_address = 0xC0A80101;  // 0xC0 = 192, 0xA8 = 168, 0x01 = 1, 0x01 = 1
 
 nrf_drv_uart_t m_uart = NRF_DRV_UART_INSTANCE(0);
 
@@ -74,52 +72,16 @@ void print_ip_address(uint32_t ip_addr) {
     uart_put_string(ip_str);
 }
 
-void dummy_function() {
-    // Простая инструкция, чтобы секция `.text` не была пустой
-    __asm__ volatile("nop");
-}
-
 int main(void) {
-    dummy_function();
     // Стираем страницу перед записью
     flash_page_erase(FLASH_START_ADDR);
 
-    // Записываем IP-адрес в первую ячейку страницы
     flash_write(FLASH_START_ADDR, ip_address);
-    flash_write(FLASH_START_ADDR, 0xC0A80001);  // 192.168.0.1 (Изменились только "A8" → "00")
-    flash_write(FLASH_START_ADDR, ip_address);
-
-    // Записываем второй IP-адрес через 4 байта
-    flash_write(FLASH_START_ADDR + 4, ip_address_2);
 
     uart_init();
     while (true) {
         nrf_delay_ms(5000);
-        uint32_t read_ip_address_1 = flash_read(FLASH_START_ADDR);
-        print_ip_address(read_ip_address_1);
-
-        uint32_t read_ip_address_2 = flash_read(FLASH_START_ADDR + 4);
-        print_ip_address(read_ip_address_2);
-
-        uint32_t const page_sz = NRF_FICR->CODEPAGESIZE;
-        uint32_t const code_sz = NRF_FICR->CODESIZE;
-
-        char buffer[32];
-        snprintf(buffer, sizeof(buffer), "Page: %lu, Code: %lu\n", (unsigned long)page_sz, (unsigned long)code_sz);
-
-        uart_put_string(buffer);
-
-        uint32_t ram_size = NRF_FICR->INFO.RAM;      // Размер RAM в килобайтах
-        uint32_t flash_size = NRF_FICR->INFO.FLASH;  // Размер Flash в килобайтах
-
-        char buffer_for_memory[64];
-        snprintf(buffer_for_memory, sizeof(buffer_for_memory), "RAM: %lu KB, Flash: %lu KB\n", (unsigned long)ram_size,
-                 (unsigned long)flash_size);
-
-        uart_put_string(buffer_for_memory);
-
-        if (read_ip_address_1 == ip_address) {
-            __NOP();
-        }
+        uint32_t read_ip_address = flash_read(FLASH_START_ADDR);
+        print_ip_address(read_ip_address);
     }
 }
